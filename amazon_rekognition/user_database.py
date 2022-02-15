9d921b553f9993363294f9783e1b07745b9eee12
sara5885 <sobeast742@gmail.com>
2022-02-15 15:19:23 +0900

    user_database.py

--- amazon_rekognition/user_database.py
+++ amazon_rekognition/user_database.py
@@ -24,7 +24,33 @@ class UserDatabase:
         self.table = db.Table(UserDatabase.__table_name)
 
     @classmethod
-    def create_table():
+    def create_table(self):  #self로 접근하는게 맞는지
+        #  table = boto3.resource(
+        #     UserDatabase.__service_name,
+        #     region_name=UserDatabase.__region_name,
+        # )
+        table = self.db.create_table(
+            TableName='Users',
+            KeySchema=[
+                {
+                    'AttributeName': 'user_id',
+                    'KeyType': 'HASH'  # Partition key
+                },
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'name',  #External Id 
+                    'AttributeType': 'S',  #string
+                },
+                {
+                    'AttributeName': 'face_id',  #External Id 
+                    'AttributeType': 'SS',
+                },
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            })
         # TableName 'User'로 설정
         # keySchema user_id
         # name: str, face_id: List[str]
@@ -39,15 +65,12 @@ class UserDatabase:
         db.Table(UserDatabase.__table_name).delete()
 
     def create(self, user_id: str, name: str):
+        
         # put_item 한 res의 status code를 확인해서 200이 아니면 exception
         pass
 
     def read(self, user_id: str) -> User:
         # item에 user 없는지 확인해서 exception -> UserDatabaseUserNotExistException
-        pass
-
-    def update_name(self, user_id: str, new_name: str):
-
         pass
 
     def delete(self, user_id: str):
