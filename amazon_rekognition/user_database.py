f3fd28d288a26f72937445108629e288990f70a9
dlawogud2993 <76619436+Jaehyung-Lim@users.noreply.github.com>
2022-02-16 15:32:08 +0900

    dlawogud2993: face_id로 검색하는 기능 아직 추가 안 했습니다.

--- amazon_rekognition/user_database.py
+++ amazon_rekognition/user_database.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional
-
+from boto3.dynamodb.conditions import Key, Attr
 import boto3
-
-from amazon_rekognition.external_id_db import ExternalIdDbNotExistException
 
 @dataclass(frozen=True)
 class User:
@@ -14,9 +12,9 @@ class User:
     @classmethod
     def parse(data: Dict):
         return User(
-            user_id=data['UserId'],
-            name=data['Name'],
-            face_ids=[tmp for tmp in data['FaceIds']],
+            user_id = data['ExternalId'],
+            name = data['Name'],
+            face_ids = data['FaceId']
         )
 
     def copy_with(
@@ -29,98 +27,6 @@ class User:
             name=name or self.name,
             face_ids=face_ids or self.face_ids,
         )
-
-
-class UserDatabase:
-    __service_name = 'dynamodb'
-    __region_name = 'ap-northeast-2'
-    __table_name = 'User'
-    __db = boto3.resource(
-        __service_name,
-        region_name=__region_name,
-    )
-
-    def __init__(self) -> None:
-        self.table = UserDatabase.__db.Table(UserDatabase.__table_name)
-
-    @classmethod
-    def create_table():
-        UserDatabase.__db.create_table(
-            TableName='Users',
-            KeySchema=[
-                {
-                    'AttributeName': 'user_id',
-                    'KeyType': 'HASH'  # Partition key
-                },
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'name',  #External Id 
-                    'AttributeType': 'S',  #string
-                },
-                {
-                    'AttributeName': 'face_id',  #External Id 
-                    'AttributeType': 'SS',
-                },
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            },
-        )
-
-    @classmethod
-    def delete_table():
-        UserDatabase.__db.Table(UserDatabase.__table_name).delete()
-
-    def create(self, user: User):
-        is_exist:bool = False
-        try:
-            self.read(User.user_id)
-        except ExternalIdDbNotExistException:
-            is_exist=False
-        else:
-            is_exist=True
-
-        table=self.__db.Table('Users')
-        res=table.put_item(Item={
-            'UserId':User.user_id,
-            'UserName':User.name,
-        })
-        # read해서 존재 여부 체크 후 이미 사용중인 user_id라면 UserDatabaseUserAlreadExistException raise
-        
-
-    def read(self, user_id: str) -> User:
-        table=self.__db.Table('Users')
-        res=table.get_item(Key={'UserID':user_id})
-
-        if not ('Item' in res):
-            raise ExternalIdDbNotExistException(user_id)
-
-        return res['Item']['UserName']
-        # item에 user 없는지 확인해서 exception -> UserDatabaseUserNotExistException
-        
-
-    def update(self, user: User):
-        try:
-            self.read(user.user_id)
-        except:
-            # TODO
-
-        self.create(user)
-    
-
-    def delete(self, user_id: str):
-        # read해서 존재 여부 체크 후 없다면 UserDatabaseUserNotExistException raise
-        pass
-
-    def search_by_name(self, name: str) -> User:
-        # name으로 user 찾기
-
-    def search_by_face_id(self, face_id: str) -> User:
-        # face_id 로 user 찾기
-
-
 
 
 class UserDatabaseException(Exception):
@@ -143,13 +49,141 @@ class UserDatabaseException(Exception):
 
 
 class UserDatabaseUserNotExistException(UserDatabaseException):
-    # TODO
+    def __init__(self, user_id) -> None:
+        super().__init__(
+            'user_id is not exist',
+            {'user_id': user_id},
+        )
 
 
 class UserDatabaseUserAlreadExistException(UserDatabaseException):
-
-    def __init__(self, user_id, str) -> None:
+    def __init__(self, user_id) -> None:
         super().__init__(
             'user_id is already exist',
             {'user_id': user_id},
         )
+
+
+class UserDatabase:
+    __service_name = 'dynamodb'
+    __region_name = 'ap-northeast-2'
+    __table_name = 'Users'
+    __db = boto3.resource(
+        __service_name,
+        region_name=__region_name,
+    )
+
+    def __init__(self) -> None:
+        self.table = UserDatabase.__db.Table(UserDatabase.__table_name)
+
+    @classmethod
+    def create_table(self):
+        UserDatabase.__db.create_table(
+            TableName='Users',
+            KeySchema=[
+                {
+                    'AttributeName': 'user_id',
+                    'KeyType': 'HASH'  # Partition key
+                },
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'user_id',  #External Id 
+                    'AttributeType': 'S',  #string
+                },
+            ],        
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            },
+        )
+
+    @classmethod
+    def delete_table():
+        UserDatabase.__db.Table(UserDatabase.__table_name).delete()
+
+    def create(self, user: User):
+        try:
+            res = self.read(user.user_id)
+        except UserDatabaseUserNotExistException:
+            self.table.put_item(
+                Item = {
+                    'user_id': user.user_id,
+                    'name': user.name,
+                    'face_ids': user.face_ids
+                }
+            )
+            return user
+        else:
+            if res['user_id'] == user.user_id:
+                raise UserDatabaseUserAlreadExistException(user.user_id)
+            
+    def read(self, user_id: str) -> User:
+        table = UserDatabase.__db.Table(UserDatabase.__table_name)
+
+        res = table.get_item(Key={'user_id': user_id}) 
+        if not('Item' in res):
+            raise UserDatabaseUserNotExistException(user_id)
+        else:
+            return res['Item']
+
+
+    #update의 case-> 없는 id일 수 있음.
+    def update(self, user: User, new_name: str):     
+        try:  
+            res = self.read(user.user_id)   
+        except UserDatabaseUserNotExistException as e: 
+            raise e
+        else:  
+            self.table.put_item(
+                Item = {
+                    'user_id': user.user_id,
+                    'name': new_name,
+                    'face_ids': user.face_ids
+                }
+            )
+            return {'user_id':user.user_id, 'name': new_name, 'face_ids': user.face_ids}
+
+    def delete(self, user_id: str):
+        # read해서 존재 여부 체크 후 없다면 UserDatabaseUserNotExistException raise
+        try:
+            res = self.read(user_id)
+        except UserDatabaseUserNotExistException as e:
+            raise e
+        else:
+            self.table.delete_item(
+                Key={
+                    'user_id': user_id
+                    }
+            )
+            return 'delete complete'
+
+    def search_by_name(self, name: str) -> User:
+        #name으로 user 찾기 
+        response = self.table.scan(
+            FilterExpression = Attr('name').eq(name)
+        )
+        items = response['Items']
+        return items
+
+    def search_by_face_id(self, face_id: str) -> User:
+        pass
+
+
+    @classmethod
+    def delete_table():
+        UserDatabase.__db.Table(UserDatabase.__table_name).delete()
+
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    user_db=UserDatabase()
+    user = User('uuid', '유재석', ['123123','111111'])
+    #res = user_db.create(user)
+    #res = user_db.update(user,'유재석')
+    #res = user_db.search_by_name('강호동')
+    res = user_db.search_by_face_id(user.face_ids)
+    #res = user_db.read('uuid')
+    #res = user_db.delete('uuid')
+    pprint(res)
