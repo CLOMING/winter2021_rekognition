595738909b77ffd5a8c6236827a46aacdab67106
anychhoice <contact@yechan.dev>
2022-02-20 15:34:03 +0900

    feat: develop tts

--- amazon_polly/README.md
+++ amazon_polly/README.md
@@ -1,2 +1,18 @@
 # AmazonPolly
 
+## Set up
+- Python `3.10.0`
+- pip3 `21.2.4`
+
+### 1. Install ffmpeg
+```bash
+# MACOS
+brew install ffmpeg
+
+# Linux
+sudo apt-get install ffmpeg libavcodec-extra
+```
+### 2. Install pydub
+```python
+pip install pydub
+```
---
+++ amazon_polly/synthesize_speech.py
@@ -0,0 +1,57 @@
+import io
+from typing import Any, Optional
+
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+class TTS():
+
+    def __init__(self, ) -> None:
+        session = Session(profile_name="default")
+        self.polly = session.client("polly")
+
+    def read(self, text: str) -> None:
+        try:
+            response = self.polly.synthesize_speech(Text=text,
+                                                    OutputFormat="mp3",
+                                                    VoiceId="Seoyeon")
+        except (BotoCoreError, ClientError) as error:
+            raise TTSException('Boto Error occured.', error)
+
+        if not "AudioStream" in response:
+            raise TTSAudioStreamNotExistException()
+
+        stream = response['AudioStream']
+        audio_data = stream.read()
+        stream.close()
+
+        sound = AudioSegment.from_file(io.BytesIO(audio_data), format="mp3")
+        play(sound)
+
+
+class TTSException(Exception):
+
+    def __init__(
+        self,
+        message: str,
+        data: Optional[Any] = None,
+    ) -> None:
+        self.message = message
+        self.data = data
+
+    def __str__(self) -> str:
+        error_message: str = f'[TTSException] {self.message}'
+
+        if self.data:
+            error_message += f'\n{self.data}'
+
+        return error_message
+
+
+class TTSAudioStreamNotExistException(TTSException):
+
+    def __init__(self) -> None:
+        super().__init__('AudioStream is not exist.')
